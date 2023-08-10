84e25d6975b64590490b1b95ea862533168c88ae
Deucalino Marildo Muvuma <muvumafernando@gmail.com>
2023-08-10 17:38:43 +0000

    starting coding structure

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -1,15 +1,18 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import "../../styles/navbar.css";
 
 export const Navbar = () => {
 	return (
 		<nav className="navbar navbar-light bg-light mb-3">
-			<Link to="/">
-				<span className="navbar-brand mb-0 h1">React Boilerplate</span>
+			<Link to="/">	
+				<button type="button" className="btn btn-p-0">
+					<img src="https://i.pinimg.com/564x/ac/44/71/ac4471f4672263c27c4dbe87da598dd2.jpg" className=" img-fluid rounded-5 w-25 h-25" alt="..."/>
+				</button>	
 			</Link>
 			<div className="ml-auto">
 				<Link to="/demo">
-					<button className="btn btn-primary">Check the Context in action</button>
+				<button type="button" class="btn btn-outline-warning text-dark w-85 h-50">       My Favorities </button>
 				</Link>
 			</div>
 		</nav>
