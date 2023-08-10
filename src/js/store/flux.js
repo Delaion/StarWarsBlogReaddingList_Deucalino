26e7de2a4e2a74c3dd6b423529fd050deb2002b4
Deucalino Marildo Muvuma <muvumafernando@gmail.com>
2023-08-10 21:38:39 +0000

    Flux and Context set

--- src/js/store/flux.js
+++ src/js/store/flux.js
@@ -19,7 +19,88 @@ const getState = ({ getStore, getActions, setStore }) => {
 			favorites:""
 		},
 		actions: {
-			// Use getActions to call a function within a fuction
+		getCharacter:()=>{
+			fetch ('https://www.swapi.tech/')
+			.then(res=>{
+				if (res.ok) throw Error(res.statusText);
+				return res.json();
+			})
+			.then(respponse=>{
+				setStore({characters:response.results});
+			})
+			.catch(error=>console.error(error));
+		},
+		getCharactersById: (id, setCharacter) => {
+			fetch(`https://www.swapi.tech/api/people/${id}`)
+			.then(res => {
+				if (!res.ok) throw Error(res.statusText);
+				return res.json();
+			})
+			.then(response=>{
+				setCharacter(response.result);
+			})
+			.catch(error=>console.error(error));
+		},
+		getPlanets: () => {
+			fetch(`https://www.swapi.tech/api/planets`)
+			.then(res => {
+				if (!res.ok) throw Error(res.statusText);
+				return res.json();
+			})
+			.then(response => {
+				setStore({ planets: response.results });
+			})
+			.catch(error => console.error(error));
+		},
+		getPlanetsById: (id, setPlanet) => {
+			fetch(`https://www.swapi.tech/api/planets/${id}`)
+			.then(res => {
+				if (!res.ok) throw Error(res.statusText);
+				return res.json();
+			})
+			.then(response => {
+				setPlanet(response.result);
+			})
+			.catch(error => console.error(error));
+		},
+		getStarships: () => {
+			fetch('https://www.swapi.tech/api/starships')
+			.then(res => {
+				if (!res.ok) throw Error(res.statusText);
+				return res.json();
+			})
+			.then(response => {
+				setStore({ starships: response.results });
+			})
+			.catch(error => console.error(error));
+		},
+		getStarshipsById: (id, setStarship) => {
+			fetch(`https://www.swapi.tech/api/starships/${id}/`)
+			.then(res => {
+				if (!res.ok) throw Error(res.statusText);
+				return res.json();
+			})
+			.then(response => {
+				setStarship(response.result);
+			})
+			.catch(error => console.error(error));
+		},
+		addFavorites: (newFavorite) => {
+			const store = getStore();
+			const favorite = store.favorites.concat(newFavorite);
+			setStore({ favorites: favorite });
+		},
+		removeFavorites: (index) => {
+			const store = getStore();
+			const favorite = store.favorites.filter((c, i) => {
+				return index !== i
+			});
+			setStore({ favorites: favorite });
+		},
+		
+		
+		
+		// Use getActions to call a function within a fuction
 			exampleFunction: () => {
 				getActions().changeColor(0, "green");
 			},
